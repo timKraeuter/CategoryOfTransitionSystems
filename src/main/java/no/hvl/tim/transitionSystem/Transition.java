f3cd7c95e0494cc0a34b685194ee1880e9e18291
Tim Kräuter <tim.kraeuterr@gmail.com>
2020-11-26 08:32:31 +0100

    Added printing to console function for debugging purposes.

--- src/main/java/no/hvl/tim/transitionSystem/Transition.java
+++ src/main/java/no/hvl/tim/transitionSystem/Transition.java
@@ -26,4 +26,9 @@ public class Transition {
     public String getLabel() {
         return label;
     }
+
+    @Override
+    public String toString() {
+        return String.format("%s --%s--> %s", source, label, target);
+    }
 }
--- src/main/java/no/hvl/tim/transitionSystem/TransitionSystem.java
+++ src/main/java/no/hvl/tim/transitionSystem/TransitionSystem.java
@@ -1,7 +1,10 @@
 package no.hvl.tim.transitionSystem;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TransitionSystem {
     private Set<State> states;
@@ -27,5 +30,22 @@ public class TransitionSystem {
 
     public Set<Transition> getTransitions() {
         return transitions;
+    }
+
+    public void printToConsole() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("States:");
+        final List<String> stateNames = this.states.stream().map(State::getName).collect(Collectors.toList());
+        builder.append(stateNames);
+        builder.append("\nTransitions:\n");
+        final Iterator<Transition> it = transitions.iterator();
+        if (it.hasNext()) {
+            builder.append(it.next());
+        }
+        while (it.hasNext()) {
+            builder.append(",\n");
+            builder.append(it.next());
+        }
+        System.out.println(builder.toString());
     }
 }
